d7f5aac6f5ee9133b1e50f16a4a429943e85386d
longhaoran <m18687430124@163.com>
2017-08-07 01:36:48 +0800

    增加geoquiz app

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,131 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mPrevButton;
+    private Button mNextButton;
+    private Question[] mQuestions = new Question[]{
+        new Question(R.string.mr_wang, true),
+        new Question(R.string.software_engineer, true),
+        new Question(R.string.effort, false),
+        new Question(R.string.africa_man, true)
+    };
+    private int mCurrentIndex = 0;
+    private TextView mQuestionTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
+        setContentView(R.layout.activity_quiz);
+//        初始化变量
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestions[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                checkAnswer(false);
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex+mQuestions.length-1)%mQuestions.length;
+                updateQuestion();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex+1)%mQuestions.length;
+                updateQuestion();
+            }
+        });
+    }
+//    在onPause onStop onDestroy之前调用
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    private void updateQuestion(){
+        int question = mQuestions[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean  answerIsTrue = mQuestions[mCurrentIndex].isAnswerTrue();
+
+        int messageId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageId = R.string.correct_toast;
+        }else{
+            messageId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
+    }
+}
