776cb2526847202bf38293168b5ffcde18d3cb6e
longhaoran <m18687430124@163.com>
2017-08-11 00:49:35 +0800

    添加作弊按钮和作弊activity

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,13 +1,54 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer)shown";
+
+    private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+//        若没有取到键对应的值，默认为false
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+
+        mShowAnswer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                }else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShowResult(true);
+            }
+        });
+    }
+
+    private void setAnswerShowResult(boolean isAnswerShown){
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
 }
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,17 +14,21 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final int REQUIST_CODE_CHEAT = 0;
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mPrevButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private Question[] mQuestions = new Question[]{
-        new Question(R.string.mr_wang, true),
-        new Question(R.string.software_engineer, true),
-        new Question(R.string.effort, false),
-        new Question(R.string.africa_man, true)
+            new Question(R.string.mr_wang, true),
+            new Question(R.string.software_engineer, true),
+            new Question(R.string.effort, false),
+            new Question(R.string.africa_man, true)
     };
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
     private TextView mQuestionTextView;
 
     @Override
@@ -41,6 +48,16 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton = (Button) findViewById(R.id.false_button);
         mPrevButton = (Button) findViewById(R.id.prev_button);
         mNextButton = (Button) findViewById(R.id.next_button);
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+
+//        作弊/查看答案按钮
+        mCheatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = newIntent(QuizActivity.this, mQuestions[mCurrentIndex].isAnswerTrue());
+                startActivityForResult(i, REQUIST_CODE_CHEAT);
+            }
+        });
 
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -67,11 +84,34 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                mCurrentIndex = (mCurrentIndex+1)%mQuestions.length;
+                mCurrentIndex = (mCurrentIndex+1)%mQuestions.length;
+                mIsCheater = false;
                 updateQuestion();
             }
         });
     }
+
+//    监听子activity的返回值
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+
+        if(requestCode == REQUIST_CODE_CHEAT) {
+            if(data == null){
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
+
 //    在onPause onStop onDestroy之前调用
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState){
@@ -121,14 +161,18 @@ public class QuizActivity extends AppCompatActivity {
     private void checkAnswer(boolean userPressedTrue){
         boolean  answerIsTrue = mQuestions[mCurrentIndex].isAnswerTrue();
 
-        int messageId = 0;
+        int messageResId = 0;
 
-        if(userPressedTrue == answerIsTrue){
-            messageId = R.string.correct_toast;
+        if(mIsCheater) {
+            messageResId = R.string.judgment_toast;
         }else{
-            messageId = R.string.incorrect_toast;
+            if(userPressedTrue == answerIsTrue){
+                messageResId = R.string.correct_toast;
+            }else{
+                messageResId = R.string.incorrect_toast;
+            }
         }
 
-        Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
